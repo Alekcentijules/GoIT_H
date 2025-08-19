9f7d9ebb6ef85d71a29c39db60f4a2e7fe701c7b
Alexcentijules <o.halaiev@gmail.com>
2025-08-19 08:22:15 +0200

    5th

--- algo/hw/task1_mod4.py
+++ algo/hw/task1_mod4.py
@@ -2,28 +2,16 @@ from pathlib import Path
 
 def total_salary(path):
     try:
-        relative_path = Path(path)
-        absolute_path = relative_path.absolute()
         salaries = []
-        total_sum = 0
-        quality = 0
-        with open(absolute_path, "r", encoding="utf-8") as file:
+        with open(Path(path), "r", encoding="utf-8") as file:
             lines = [el.strip() for el in file.readlines()]
-            print(lines)
-            for line in lines:
-                num = ""
-                for el in line:
-                    if el in "0123456789":
-                        num = num + el
-                salaries.append(num)
-        for el in salaries:
-            total_sum += int(el)
-            quality = quality + 1
-        average_sum = total_sum / quality
-        result = (total_sum, average_sum)
+            salaries = [int(line.split(",")[1]) for line in lines]
+        average_sum = sum(salaries) / len(salaries)
     except FileNotFoundError:
         return f"File {path} not found!"
-    return result
+    except (ValueError, IndexError):
+        return "File format is incorrect!"
+    return (sum(salaries), average_sum)
 
 
 filename = input("Enter a filename: ")
