197fc997ac61da7aa11394bae72e8d33f05591a2
Alexcentijules <o.halaiev@gmail.com>
2025-08-19 08:19:51 +0200

    4th

--- algo/hw/task1_mod4.py
+++ algo/hw/task1_mod4.py
@@ -1,35 +1,32 @@
 from pathlib import Path
 
-def search_path(filename):
-    relative_path = Path(filename)
-    absolute_path = relative_path.absolute()
-    return absolute_path
-
 def total_salary(path):
-    salaries = []
-    total_sum = 0
-    quality = 0
-    average_sum = total_sum / quality
-    result = (total_sum, average_sum)
-    with open(path, "r", encoding="utf-8") as file:
-        lines = [el.strip() for el in file.readlines()]
-        print(lines)
-        for line in lines:
-            num = ""
-            for el in line:
-                if el in "0123456789":
-                    num = num + el
-            salaries.append(num)
-    for el in salaries:
-        total_sum += int(el)
-        quality = quality + 1
-    
+    try:
+        relative_path = Path(path)
+        absolute_path = relative_path.absolute()
+        salaries = []
+        total_sum = 0
+        quality = 0
+        with open(absolute_path, "r", encoding="utf-8") as file:
+            lines = [el.strip() for el in file.readlines()]
+            print(lines)
+            for line in lines:
+                num = ""
+                for el in line:
+                    if el in "0123456789":
+                        num = num + el
+                salaries.append(num)
+        for el in salaries:
+            total_sum += int(el)
+            quality = quality + 1
+        average_sum = total_sum / quality
+        result = (total_sum, average_sum)
+    except FileNotFoundError:
+        return f"File {path} not found!"
     return result
 
 
 filename = input("Enter a filename: ")
-absolute_path = search_path(filename)
-salaries = total_salary(absolute_path)
+salaries = total_salary(filename)
 
-print(f"It's absolute path: {absolute_path}")
 print(salaries)
