601a4890c2a0b63fc11f19ab0aeee7979f3817c1
Alexcentijules <o.halaiev@gmail.com>
2025-08-21 14:18:52 +0200

    7th

--- algo/hw/task1_mod4.py
+++ algo/hw/task1_mod4.py
@@ -24,18 +24,29 @@ from pathlib import Path
 #  C:\Users\ohala\VSCodeProjects\GoIT_H\algo\hw\cats_file.txt
 
 def get_cats_info(path):
-    cats = []
-    print(f"Path received: '{path}'")
-    with open(Path(path), "r") as file:
-        for line in file:
-            print(line)
-            id, name, age = line.strip().split(",")
-            cat = {"id":id, "name":name, "age":age}
-            cats.append(cat)
+    try:
+        cats = []
+        broken_lines = []
+        with open(Path(path), "r", encoding="utf-8") as file:
+            for line in file:
+                parts = line.strip().split(",")
+                if len(parts) == 3:
+                    id, name, age = parts
+                    cat = {"id":id, "name":name, "age":age}
+                    cats.append(cat)
+                else:
+                    broken_lines.append(parts)
+                # except (ValueError, IndexError):
+                #     return f"File format is incorrect!" 
+            
+    except FileNotFoundError:
+         return f"File {path} not found!"
     if not cats:
-            return f"File {path} is empty!"
-    return cats
+                return f"File {path} is empty!"
+    elif broken_lines:
+        return f"Your cats info: {{'cats': cats, 'broken_lines': broken_lines}}"
+    return f"Your cats info: {cats}"
 
 cats_file = input("Enter a cats file: ")
-cats_info = get_cats_info(cats_file)
+cats_info = get_cats_info(cats_file) 
 print(cats_info)
