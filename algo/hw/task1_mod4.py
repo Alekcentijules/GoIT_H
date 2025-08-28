0db14fb080695cc7532bb5d46040a3a5028b8532
Alexcentijules <o.halaiev@gmail.com>
2025-08-28 18:31:01 +0200

    9th

--- algo/hw/task1_mod4.py
+++ algo/hw/task1_mod4.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import colorama
+import os
 
 # def total_salary(path):
 #     try:
@@ -8,7 +10,7 @@ from pathlib import Path
 #         with open(Path(path), "r", encoding="utf-8") as file:
 #             for line in file:
 #                 name, salary = line.strip().split(",")
-#                 salaries.append(int(salary))
+#                 salaries.append(int(salary)) 
 #         average_sum = sum(salaries) / len(salaries)
 #     except FileNotFoundError:
 #         return f"File {path} not found!"
@@ -23,19 +25,39 @@ from pathlib import Path
 
 #  C:\Users\ohala\VSCodeProjects\GoIT_H\algo\hw\cats_file.txt
 
-def get_cats_info(path):
-    try:
-        cats = []
-        with open(path, "r", encoding="utf-8") as file:
-            for line in file:
-                parts = line.strip().split(",")
-                if len(parts) == 3:
-                    id, name, age = parts
-                    cats.append({"id":id, "name":name, "age":age})
-    except FileNotFoundError:
-         return []
-    return cats
+# def get_cats_info(path):
+#     try:
+#         cats = []
+#         with open(path, "r", encoding="utf-8") as file:
+#             for line in file:
+#                 parts = line.strip().split(",")
+#                 if len(parts) == 3:
+#                     id, name, age = parts
+#                     cats.append({"id":id, "name":name, "age":age})
+#     except FileNotFoundError:
+#          return []
+#     return cats
 
-cats_file = input("Enter a cats file: ")
-cats_info = get_cats_info(cats_file) 
-print(cats_info)
+# cats_file = input("Enter a cats file: ")
+# cats_info = get_cats_info(cats_file) 
+
+# print(cats_info)
+
+print(f"This directory: {os.getcwd()}")
+
+def show_directory(path):
+    directory = Path(path)
+
+    for p in directory.iterdir():
+        # if p.exists():
+        #     print(f"{p} is exist")
+        # elif p.is_dir():
+        #     print(f"{p} is a directory")
+        # elif p.is_file():
+        #     print(f"{p} is a file")
+        print(p)
+
+path = input("Enter a path of directory: ")
+directory = show_directory(path)
+
+print(directory)
