23de8a4bf4bbd36c9c83ccc63a259f87361e326e
Alexcentijules <o.halaiev@gmail.com>
2025-09-04 13:38:48 +0200

    10th

--- algo/hw/task1_mod4.py
+++ algo/hw/task1_mod4.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-import colorama
-import os
+from colorama import init, Fore, Style
+import sys
 
 # def total_salary(path):
 #     try:
@@ -43,21 +43,42 @@ import os
 
 # print(cats_info)
 
-print(f"This directory: {os.getcwd()}")
 
-def show_directory(path):
-    directory = Path(path)
+init(autoreset=True)
 
-    for p in directory.iterdir():
-        # if p.exists():
-        #     print(f"{p} is exist")
-        # elif p.is_dir():
-        #     print(f"{p} is a directory")
-        # elif p.is_file():
-        #     print(f"{p} is a file")
-        print(p)
+def show_directory(path, indent=0):
+    try:
+        directory = Path(path).resolve()
 
-path = input("Enter a path of directory: ")
-directory = show_directory(path)
+        if not directory.exists():
+            print(f"{Fore.RED} Error: Path {directory} not exist!")
+            return
+        if not directory.is_dir():
+            print(f"{Fore.RED} Error: {directory} is not a directory!")
+            return
+        
+        for item in directory.iterdir():
+            indent_str = " " * indent
 
-print(directory)
+            if item.is_dir():
+                print(f"{indent_str}{Fore.BLUE}📂 {item.name}{Style.RESET_ALL}")
+                show_directory(item, indent + 1)
+            else:
+                print(f"{indent_str}{Fore.GREEN}📜 {item.name}{Style.RESET_ALL}")
+        
+    except PermissionError:
+        print(f"{Fore.RED} Error: not access to {path}")
+    except Exception as e:
+        print(f"{Fore.RED} There was an error: {str(e)}")
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"{Fore.RED} Error: Specify the path to the directory!")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    print(f"Directory structure: {path}")
+    show_directory(path)
+
+if __name__ == "__main__":
+    main()
