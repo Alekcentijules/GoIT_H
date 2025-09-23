06b20b6c9e957e41883eb709beeb468f5ae186da
Alexcentijules <o.halaiev@gmail.com>
2025-09-23 08:50:21 +0200

    13th

--- algo/hw/04/t4/bot__/command_parser.py
+++ algo/hw/04/t4/bot__/command_parser.py
@@ -4,4 +4,6 @@ def parse_input(user_input):
     values = user_input.split()
     if len(values) == 2:
         return cmd, values[1]
+    elif values[2] not in "0123456789":
+        return "When adding a phone number, only digits should be entered."
     return cmd, *args
--- algo/hw/04/t4/main.py
+++ algo/hw/04/t4/main.py
@@ -2,28 +2,37 @@ from bot__ import parse_input
 from bot__ import add_contact, change_contact, output_phone
 
 def main():
-    contacts = {}
-    print("Welcome to the assistant bot!")
-    while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can i help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            command, arg = parse_input(user_input)
-            print(output_phone(arg, contacts))
-        elif command == "all":
-            for name, phone in contacts.items():
-                print(name, phone)
-        else:
-            print("Invalid command!")
+    # try:
+        contacts = {}
+        print("Welcome to the assistant bot!")
+        while True:
+            user_input = input("Enter a command: ")
+            command, *args = parse_input(user_input)
+            print(parse_input(user_input))
+            print(command, *args)
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                break
+            elif command == "hello":
+                print("How can i help you?")
+            elif command == "add":
+                print(add_contact(args, contacts))
+            elif command == "change":
+                print(change_contact(args, contacts))
+            elif command == "phone":
+                try:    
+                    command, arg = parse_input(user_input)
+                    print(output_phone(arg, contacts))
+                except KeyError as err:
+                    print(f"This contact isn't in list: {err}")
+            elif command == "all":
+                for name, phone in contacts.items():
+                    print(name, phone)
+            else:
+                print("Invalid command!")
+    # except KeyError as err:
+    #     print(f"This contact isn't in list. {err}")
+        
 
 if __name__ == "__main__":
     main()
