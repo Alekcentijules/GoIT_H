7ab173680f9d9688700ead9c951a6f868a0c859a
Alexcentijules <o.halaiev@gmail.com>
2025-09-23 09:42:53 +0200

    15th

--- algo/hw/04/t4/bot__/processor_functions.py
+++ algo/hw/04/t4/bot__/processor_functions.py
@@ -9,8 +9,14 @@ def add_contact(args, contacts):
 
 def change_contact(args, contacts):
     name, phone = args
+    if not name in contacts:
+        return "This contact isn't in list."
+    elif not phone.isdigit():
+        return "When adding a phone number, only digits should be entered."
     contacts[name] = phone
     return "Contact changed!"
 
 def output_phone(arg, contacts):
+    if not arg in contacts:
+        return "This contact isn't in list."
     return contacts[arg]
